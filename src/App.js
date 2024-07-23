3cf26dc9ced56fef80f2de8d0b7be298d8a5d117
Aviral Sharma <aviral16072000@gmail.com>
2024-07-23 17:56:36 +0530

    Redy for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import BlogList from './components/BlogList';
+import BlogDetail from './components/BlogDetail';
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const[posts,setPosts] = useState([]);
+
+  return(
+    <Router basename="/react-blog">
+      <Routes>
+        <Route path = "/" element={<BlogList posts={posts} setPosts={setPosts}/>}/>
+        <Route path="/post/:id" element={<BlogDetail posts={posts}/>}/>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
